da212b1c35669a5a426d46023fba3cb63f9927e8
sunxiaoou <sun_xiaoou@qq.com>
2024-06-20 12:48:00 +0800

    add tests.py

--- drf/app/tests.py
+++ drf/app/tests.py
@@ -6,11 +6,14 @@ import requests
 from django.test import TestCase
 
 
-# Create your tests here.
 class DrfTestCase(TestCase):
-    def setUp(self) -> None:
-        self.base_url = "http://127.0.0.1:8000"
-        url = f"{self.base_url}/login/"
+    base_url = "http://127.0.0.1:8000"
+    csrf_token = None
+    last_id = -1
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        url = f"{cls.base_url}/login/"
         payload = json.dumps({
             "username": "sun_xo",
             "password": "sun_xo"
@@ -22,8 +25,18 @@ class DrfTestCase(TestCase):
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         response.raise_for_status()
-        self.csrf_token = response.json()['token']
-        print("csrf_token(%s)" % self.csrf_token)
+        cls.csrf_token = response.json()['token']
+        print("csrf_token(%s)" % cls.csrf_token)
+
+        url = f"{cls.base_url}/books/api/"
+        headers = {
+            'x-csrftoken': cls.csrf_token
+        }
+        response = requests.request("GET", url, headers=headers, data={})
+        response.raise_for_status()
+        books = response.json()['data']
+        cls.last_id = max([book['id'] for book in books])
+        print("last_id(%d)" % cls.last_id)
 
     def test_list_books(self):
         print("Test - list books")
@@ -45,3 +58,43 @@ class DrfTestCase(TestCase):
         response.raise_for_status()
         print(response.json()['data'])
 
+    def test_add_book(self):
+        print("Test - add book")
+        url = f"{self.base_url}/books/api/"
+        payload = json.dumps({
+            "title": "镜之孤城",
+            "price": "56.00",
+            "pub_date": "2020-04-01"
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'x-csrftoken': self.csrf_token
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response.raise_for_status()
+        print(response.text)
+
+    def test_delete_book(self):
+        print("Test - delete book")
+        url = f"{self.base_url}/book/api/" + str(self.last_id)
+        headers = {
+            'x-csrftoken': self.csrf_token
+        }
+        response = requests.request("DELETE", url, headers=headers, data={})
+        print(response.text)
+
+    def test_modify_book(self):
+        print("Test - modify book")
+        url = f"{self.base_url}/book/api/3"
+        payload = json.dumps({
+            "title": "夜晚的潜水艇",
+            "price": "52.00",
+            "pub_date": "2020-09-09"
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'x-csrftoken': self.csrf_token
+        }
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        response.raise_for_status()
+        print(response.text)
